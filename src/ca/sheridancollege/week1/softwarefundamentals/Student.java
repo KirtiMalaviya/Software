75ce1b566bce489385ba2f8649983d5826e0f8ec
KirtiMalaviya <malaviki@sheridancollege.ca>
2024-05-17 10:20:39 -0400

    featch

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -11,6 +11,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * each student can: say their name (getName)
  * push operation
  * changing 
+ featch and 
  * @author dancye May 7, 2018
  */
 public class Student 
